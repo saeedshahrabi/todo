d65bb818e6b60e021cc221c8b9b7f4bc1fafb7ee
saeed <44498907+saeedshahrabi@users.noreply.github.com>
2020-04-29 02:52:27 +0430

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TodoMain } from './TodoForm/TodoMain';
+class App extends React.Component {
+  render(){
+  return (
+    <div>
+      <TodoMain />
+    </div>
+  );
+}
+}
+export default App;
